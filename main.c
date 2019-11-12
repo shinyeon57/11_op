acd034a3e8036f3d15b9e8ae5eb2d762580b6ef7
shinyeon57 <noeys57@sookmyung.ac.kr>
2019-11-12 15:21:21 +0900

    practice 02

--- main.c
+++ main.c
@@ -4,21 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	
-	char*pc;
-	int *pi;
-	double *pd;
-	
-	pc = (char *)10000;
-	pi = (int*)10000;
-	pd = (double *)10000;
-	
-	printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
-	
-	pc++;
-	pi++;
-	pd++;
-	printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
-	
+		
+	int i = 10;
+	int *ptr = &i;
+	printf("%i = %d, pi : %p\n", i, ptr);
+	(*ptr)++;
+	printf("%i = %d, pi : %p\n", i, ptr);
+	*ptr++;
+	printf("%i = %d, pi : %p\n", i, ptr);
+
 	return 0;
 }
